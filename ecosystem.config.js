8d6a0b254b8e9e7b9a69041cd28b2516ab049eea
秦钰奇 <121993898+APieceOfToast123@users.noreply.github.com>
2024-09-14 20:35:09 -0700

    Create ecosystem.config.js

---
+++ ecosystem.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    apps: [{
+      name        : "nuxt-app",
+      cwd         : "/home/lighthouse/nuxtjs-tailwindcss-portfolio",
+      script      : "/usr/bin/yarn",
+      args        : "start",
+      interpreter : "/bin/bash",
+      env: {
+        NODE_ENV: "production",
+        HOST: "0.0.0.0",
+        PORT: 3000
+      }
+    }]
+  }
+  
