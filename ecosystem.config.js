803ec806e0655a3ca209f3607d5b6b57d52b44db
秦钰奇 <121993898+APieceOfToast123@users.noreply.github.com>
2024-09-17 00:54:06 -0700

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,15 +1,14 @@
 module.exports = {
-    apps: [{
-      name        : "nuxt-app",
-      cwd         : "/home/lighthouse/nuxtjs-tailwindcss-portfolio",
-      script      : "/usr/bin/yarn",
-      args        : "start",
-      interpreter : "/bin/bash",
-      env: {
-        NODE_ENV: "production",
-        HOST: "0.0.0.0",
-        PORT: 3030
-      }
-    }]
-  }
-  
+  apps: [{
+    name        : "nuxt-app",
+    cwd         : "/home/lighthouse/nuxtjs-tailwindcss-portfolio",
+    script      : "/home/lighthouse/.nvm/versions/node/v16.20.2/bin/yarn", 
+    args        : "start",
+    interpreter : "/bin/bash",
+    env: {
+      NODE_ENV: "production",
+      HOST: "0.0.0.0",
+      PORT: 3030
+    }
+  }]
+}
