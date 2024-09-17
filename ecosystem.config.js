24f5677104115597b8599c87d77e9a4aa2ff7fb6
秦钰奇 <121993898+APieceOfToast123@users.noreply.github.com>
2024-09-17 01:13:05 -0700

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,9 +2,8 @@ module.exports = {
   apps: [{
     name        : "nuxt-app",
     cwd         : "/home/lighthouse/nuxtjs-tailwindcss-portfolio",
-    script      : "/home/lighthouse/.nvm/versions/node/v16.20.2/bin/yarn", 
+    script      : "npm",
     args        : "start",
-    interpreter : "/bin/bash",
     env: {
       NODE_ENV: "production",
       HOST: "0.0.0.0",
